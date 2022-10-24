836280c5b8a487078f94fa14334b3e373454ac20
s2834545 <p.chen-2@student.utwente.nl>
2022-10-24 09:59:21 +0200

    consultant: master key gen, client key gen

--- Client.py
+++ Client.py
@@ -1,9 +1,32 @@
-
+from Consultant import Consultant
 
 class Client():
-    def __init__(self, id:int) -> None:
+    def __init__(self, id : int, consultant : Consultant) -> None:
         self.id = id
+        self.consultant = consultant
 
-    def getId(self):
+        # get private key 
+        self.key = self.consultant.ken_gen(self.id)
+
+    def getId(self) -> int:
+        """ Function to retrieve the id of the client
+
+        Returns:
+            int: the client id
+        """
         return self.id
+
+    def get_key(self) -> bytes:
+        """ Function to retrieve the private key of the client
+
+        Returns:
+            bytes: the client's private key
+        """
+        return self.key
+
+    def write(message : str):
+        pass
+
+    def search(self, keyword : str):
+        pass
     
--- Consultant.py
+++ Consultant.py
@@ -1,7 +1,29 @@
-
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA512
+from Crypto.Random import get_random_bytes
 
 class Consultant():
     def __init__(self) -> None:
-        pass
+        # security parameter
+        sec_param = 2 ** 6
+
+        # generate master key
+        self.master_key = get_random_bytes(sec_param)
+        print(self.master_key)
 
     
+    # https://pycryptodome.readthedocs.io/en/latest/src/protocol/kdf.html
+    # it's pretty slow
+    def ken_gen(self, client_id : int) -> bytes:
+        salt = get_random_bytes(16)
+        key = PBKDF2(self.master_key, salt, 32, count=1000000, hmac_hash_module=SHA512)
+        
+        return key
+    
+
+    def write(self, client_id : int):
+        pass
+    
+
+    def search(self, client_id : int, keyword : str):
+        pass
