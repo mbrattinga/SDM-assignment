7b3dbae8c3b386a1e0f5617085cbe7ddb28e3c72
s2834545 <p.chen-2@student.utwente.nl>
2022-10-24 15:52:33 +0200

    Added encrypted search

--- Database.py
+++ Database.py
@@ -1,14 +1,38 @@
-
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA512
 
 
 class Database():
     def __init__(self) -> None:
-        pass
+        self.storage = {}
+        self.doc_counter = 0
 
     def add(self):
         pass
 
-    def search(self):
-        print("I am not going to search...")
+    def search(self, enc_keyword : bytes, token : bytes) -> [int]:
+        """ Perform search of a encrypted keyword
+
+        Args:
+            enc_keyword (bytes): the encrypted keyword
+            token (bytes): search token
+
+        Returns:
+            [int]: return the list of document indexes
+        """
+
+        ret = []
+        
+        for index, keyword_list in self.storage.items():
+            for keyword in keyword_list:
+                xored = keyword ^ enc_keyword
+
+                left = xored[:12]
+                right = PBKDF2(token, "", 4, count=1000, hmac_hash_module=SHA512)
+                if (right == xored[12:16]):
+                    ret.append(index)
+
+
+        return ret
 
     
