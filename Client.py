d3ff367513110a46cccebaf1b8d63df902555907
Martijn <m.brattinga@student.utwente.nl>
2022-10-24 18:04:42 +0200

    not working yet...

--- Client.py
+++ Client.py
@@ -4,11 +4,14 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2
+import sys
+from Database import Database
 
 class Client():
-    def __init__(self, id : int, consultant : Consultant) -> None:
+    def __init__(self, id : int, consultant : Consultant, database : Database) -> None:
         self.id = id
         self.consultant = consultant
+        self.database = database
 
         self.key = self.consultant.key_gen(self.id) # get private key 
 
@@ -29,35 +32,52 @@ class Client():
         return self.key
 
     def write(self, keywords):
-        cipher_AES = AES.new(self.key, AES.MODE_ECB)
+        cipher_AES = AES.new(self.key, AES.MODE_ECB) #deterministic encryption
+        s_cipher = AES.new(self.key, AES.MODE_CTR)
+        hmac_key = HMAC.new(bytes(self.key), digestmod=SHA512)
 
         C = list()
         for i in range(len(keywords)):
-            X_i = cipher_AES.encrypt(pad(bytes(keywords[i], 'utf-8'), 16))
+            W_i = keywords[i]
+            X_i = cipher_AES.encrypt(pad(bytes(W_i, 'utf-8'), AES.block_size))
+            print("Encrypted keyword",i, X_i)
+            # print("Encrypted keyword",i, int.from_bytes(X_i, byteorder='big'))
+            S_i = s_cipher.encrypt(pad(bytes(i), AES.block_size))[:12] # is this secure?
 
-            hmac = HMAC.new(bytes(self.key), digestmod=SHA512)
-            k_i = hmac.update(X_i[:12]).digest()
+            hmac_key = HMAC.new(bytes(self.key), digestmod=SHA512)
+            k_i = hmac_key.update(X_i[:12]).digest()
             # T_i = S_i || F_k(S_i)
-            T_i = get_random_bytes(12) + PBKDF2(k_i, "", 4, hmac_hash_module=SHA512)
-            C.append((int.from_bytes(X_i, byteorder='big') ^ int.from_bytes(T_i, byteorder='big')).to_bytes(16, byteorder='big'))
+            T_i = S_i + PBKDF2(k_i, S_i, 4, hmac_hash_module=SHA512)
+            C.append(bytes(a ^ b for a,b in zip(X_i, T_i)))
 
+        print("Calculated ciphertext keywords, sending to database...")
+        self.database.add(C)
         return C
 
-    def decrypt(self, keywords):
-        cipher_AES = AES.new(self.key, AES.MODE_ECB)
+    # Not working yet
+    # def decrypt(self, keywords):
+    #     s_cipher = AES.new(self.key, AES.MODE_CTR)
+    #     hmac_key = HMAC.new(bytes(self.key), digestmod=SHA512)
 
-        W = list()
-        for i in range(len(keywords)):
-            C_i = keywords[i]
+    #     W = list()
+    #     for i in range(len(keywords)):
+    #         C_i = keywords[i]
             
-            hmac = HMAC.new(bytes(self.key), digestmod=SHA512)
-            k_i = hmac.update(X_i[:12]).digest()
-            # T_i = S_i || F_k(S_i)
-            T_i = 0
-            W.append((int.from_bytes(C_i, byteorder='big') ^ int.from_bytes(T_i, byteorder='big')).to_bytes(16, byteorder='big'))
+    #         S_i = s_cipher.encrypt(pad(bytes(i), AES.block_size))
+    #         k_i = hmac_key.update(X_i[:12]).digest()
+    #         T_i = s_hmac.update(i).digest()
+    #         W.append(bytes(a ^ b for a,b in zip(C_i, T_i)))
 
-        return W
+    #     return W
 
     def search(self, keyword : str):
-        pass
+        cipher_AES = AES.new(self.key, AES.MODE_ECB) #deterministic encryption
+        encrypted_keyword = cipher_AES.encrypt(pad(bytes(keyword, 'utf-8'), AES.block_size))
+
+        hmac_key = HMAC.new(bytes(self.key), digestmod=SHA512)
+        token = hmac_key.update(encrypted_keyword[:12]).digest()
+
+        print("Searching for keyword ", encrypted_keyword, "with token", token)
+        self.database.search(encrypted_keyword, token)
+        
     
--- Database.py
+++ Database.py
@@ -1,14 +1,39 @@
 
 
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA512
 
 class Database():
     def __init__(self) -> None:
-        pass
+        self.storage = {}
+        self.doc_counter = 0
 
-    def add(self):
-        pass
+    def add(self, c_keywords):
+        self.storage[self.doc_counter] = c_keywords
+        print("Database: added new document with keywords, with ID", self.doc_counter)
+        self.doc_counter += 1
 
-    def search(self):
-        print("I am not going to search...")
+    def search(self, enc_keyword : bytes, token : bytes):
+        """ Perform search of a encrypted keyword
+        Args:
+            enc_keyword (bytes): the encrypted keyword
+            token (bytes): search token
+        Returns:
+            [int]: return the list of document indexes
+        """
+
+        ret = []
+        
+        for index, keyword_list in self.storage.items():
+            for keyword in keyword_list:
+                xored = bytes(a ^ b for a,b in zip(keyword, enc_keyword))
+
+                left = xored[:12]
+                right = PBKDF2(token, left, 4, count=1000, hmac_hash_module=SHA512)
+                if (right == xored[12:16]):
+                    ret.append(index)
+
+
+        return ret
 
     
