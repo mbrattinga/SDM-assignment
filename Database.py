1e92063febb9e868692e7c7fe69445a3b2fa5293
s2834545 <p.chen-2@student.utwente.nl>
2022-11-03 18:22:50 +0100

    first scatch of delete

--- Database.py
+++ Database.py
@@ -4,3 +4,63 @@ class Database():
         # array(s) stored in the server
         # should be an array for each client (?)
         self.array = [bytes]
+
+    def hash2(token, random):
+        return
+
+    def delete(index, ctxt, token):
+        
+        # 1
+        token1, token2, token3, doc_id = token
+
+        T_d = [] # to be removed
+        if token1 not in T_d:
+            return
+
+        # 2
+        alfa_1_prime = T_d[token1] ^ token2
+
+        # 3
+        f = [] # to be removed
+        for i in range(1, len(f) + 1):
+
+            # a
+            A_d # to be removed
+            D_i, r = A_d[alfa_1_prime]
+            alfa1, alfa2, alfa3, alfa4, alfa5, alfa6, mu = D[i] ^ hash2(token3, r)
+
+            # b
+            A_d[alfa_1_prime] = rand
+
+            # c
+            T_s = [] # to be removed
+            gamma, zero = T_s[free] # zero TODO
+
+            # d
+            T_s[free] = alfa4, zero
+
+            # e
+            A_s = []
+            A_s[alfa4]  = gamma, alfa_1_prime
+
+            # f
+            N_minus1 = something # TODO
+            beta1, beta2, r_minus1 = A_s[alfa5]
+            A_s[alfa5] = beta1, beta2 ^ alfa4 ^ alfa6, r_minus1
+
+            beta1, beta2, beta3, beta4, beta5, beta6, mu_star, r_star_minus1 = A_s[alfa2]
+            A_s[alfa2] = beta1, beta2, beta3 ^ alfa_1_prime ^ alfa3, beta4, beta5, beta6 ^ alfa4 ^ alfa6, mu_star, r_star_minus1
+
+            # g
+            N_plus1 = something # TODO
+            beta1, beta2, beta3, beta4, beta5, beta6, mu_star, r_star_plus1 = A_s[alfa3]
+            A_s[alfa3] = beta1, beta2 ^ alfa_1_prime ^ alfa2, beta3, beta4, beta5 ^ alfa4 ^ alfa5, beta6, mu_star, r_star_plus1
+
+            # h
+            alfa_prime_i_plus_1 = alfa1
+
+        # 4
+
+
+        # 5
+
