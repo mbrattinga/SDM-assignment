fb720889d9c6a0357633505262d7d049327765f8
= <=>
2022-11-07 17:23:52 +0100

    setup of search

--- Database.py
+++ Database.py
@@ -1,4 +1,9 @@
+import math
+from Crypto.Hash import SHA256
+
+
 class Database():
+
     def __init__(self) -> None:
 
         # # array(s) stored in the server
@@ -16,5 +21,52 @@ class Database():
             raise Exception("The database has been set-up before, cannot do that twice!")
 
     def search(self, search_token):
-        (Fw, Gw, Pw) = search_token
         print("Starting search on database...")
+        zeros = bytes("0" * int(math.ceil(math.log((len(self.A_s))))), 'utf-8')
+
+        # Parse search token
+        (tau_1, tau_2, tau_3) = search_token
+        
+
+        files = list()
+
+        # Return empty list if tau1 not in search table
+        if not tau_1 in self.T_s:
+            return []
+
+         # step 2
+        # recover pointer to first node of list
+        alpha_1 = bytes(a ^ b for a,b in zip(self.T_s[tau_1], tau_2))
+
+        # step 3
+        # look up N1
+        while True:
+            N_1 = self.A_s[alpha_1]
+            (v_1, r_1) = N_1
+
+            H1 = SHA256.new(tau_3 + r_1).digest()
+            x = bytes(a ^ b for a,b in zip(v_1, H1)) # x = id_1 || 0 || addr_s_N+
+
+            files.append(x) # TODO only append the id
+
+        return files
+
+
+
+
+
+        # # step 4
+        # # repeat step 3 until address in the tuple is zero
+        # # this gets all nodes for the search keyword
+        # while addrs != 0:
+        #     n_decrypted.append(decr_todo(n[-1], tau_3))
+        #     v1, r1 = n[-1]
+        #     (id, 0, addrs) = xor_todo (v1, hmac_fn(tau_3, r1))
+        #     n.append(addrs)
+        #     id.append(id)
+
+        # # for all ids found, return the encrypted documents/locations
+        # c_filtered = [c[id_item] for id_item in id]
+
+        # return c_filtered
+        
